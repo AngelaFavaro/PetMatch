ca6fb22870581db836c51cb9a7f66ede069c83c5
Linor <lindorlinor@gmail.com>
2025-12-30 12:41:21 +0100

    continuo pagina

--- src/php/admin/dettagli-richiesta.php
+++ src/php/admin/dettagli-richiesta.php
@@ -11,18 +11,20 @@ $title = '<title>Area riservata admin - PetMatch </title>';
 $description = '<meta name="description" content="Area riservata per gli amministratori di PetMatch">';
 $keywords = "";
 
-
+$_richiesta['id']=1; // TODO da DB
+$scarta_richiesta = '<a data-id-richiesta="'.$_richiesta['id'].'" class="orange-button" >Scarta richiesta</a>';
 $nav = file_get_contents('./src/template/partials/nav-admin.html');
 
 $breadcrumb = getBreadcrumb('dettagli-richiesta', $pagine);
 
 $main = file_get_contents('./src/template/main/admin/dettagli-richiesta.html');
 
-$paginaHTML = str_replace('[title]', $title, $paginaHTML);
 // $paginaHTML = str_replace('[description]', $description, $paginaHTML);
 // $paginaHTML = str_replace('[keywords]', $keywords, $paginaHTML);
 $paginaHTML = str_replace('[breadcrumb]', $breadcrumb, $paginaHTML);
+$paginaHTML = str_replace('[title]', $title, $paginaHTML);
 $paginaHTML = str_replace('[nav]', $nav, $paginaHTML);
+$main = str_replace('[scarta-richiesta]', $scarta_richiesta, $main);
 $paginaHTML = str_replace('[main]', $main, $paginaHTML);
 
 echo $paginaHTML;
