1ebff1202403fd514539d597c5a7cc0a28ac62d7
Lindor Linor <lindorlinor@gmail.com>
2026-01-02 18:54:41 +0100

    inizio tabella statica di nuove richieste

--- src/php/admin/richieste-adozione.php
+++ src/php/admin/richieste-adozione.php
@@ -1,20 +1,29 @@
 <?php
-include './src/utils.php';
-include './src/DBconnection.php';
-use DB\DBAccess;
-$_SESSION['user'] = 'lindorlinor@gmail.com';
 
     if(isset($_GET['id-animale']) && isset($_GET['email']) ) {
         require './src/php/admin/dettagli-richiesta.php';
-
+        
     }else{
+        include './src/utils.php';
+        include './src/DBconnection.php';
+        // use DB\DBAccess;
+        $_SESSION['user'] = 'lindorlinor@gmail.com';
+
+
         // echo 'Qui ci va la pagina delle richieste di adozione, quando metti "<h1>?email=lindorlinor@gmail.com&id-animale=1</h1>" ti apre la singola richiesta (obv metti i valori che vuoi nei parametri)';
+        
         $paginaHTML = loadTemplate('./src/template/layout-admin.html', '<p>Errore: template layout.html non trovato o non leggibile.</p>');
         $breadcrumb = getBreadcrumb('richieste-adozione', $pagine);
         $nav = buildAdminNav($adminMenu,'./richieste-adozione');
 
         $main = loadTemplate('./src/template/main/admin/richieste-adozione.html');
 
+        $title = "<title>Richieste di Adozione - PetMatch</title>";
+        $description = "<meta name='description' content='Visualizza e gestisci le richieste di adozione degli animali presenti su PetMatch.'>";
+        $keywords = "<meta name='keywords' content='richieste, adozione, animali, PetMatch'>";
+        $paginaHTML = str_replace('[title]', $title, $paginaHTML);
+        $paginaHTML = str_replace('[description]', $description, $paginaHTML);
+        $paginaHTML = str_replace('[keywords]', $keywords, $paginaHTML);
         $paginaHTML = str_replace('[breadcrumb]', $breadcrumb, $paginaHTML);
         $paginaHTML = str_replace('[nav]', $nav, $paginaHTML);
         $paginaHTML = str_replace('[main]', $main, $paginaHTML);
