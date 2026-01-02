2ac94c19dee4b7f128d2e0faf42ad3ff82c06b2d
Lindor Linor <lindorlinor@gmail.com>
2026-01-02 01:31:22 +0100

    reso messaggio più chiaro

--- src/php/admin/richieste-adozione.php
+++ src/php/admin/richieste-adozione.php
@@ -4,7 +4,7 @@
         require './src/php/admin/dettagli-richiesta.php';
 
     }else{
-        echo 'Qui ci va la pagina delle richieste di adozione, quando metti "?email=lindorlinor@gmail.com&id-animale=1" ti apre la singola richiesta (obv metti i valori che vuoi nei parametri)';
+        echo 'Qui ci va la pagina delle richieste di adozione, quando metti "<h1>?email=lindorlinor@gmail.com&id-animale=1</h1>" ti apre la singola richiesta (obv metti i valori che vuoi nei parametri)';
 
     }
 
