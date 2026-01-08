c8b9174dfb644dca2457599e522a04a39301c542
Lindor Linor <lindorlinor@gmail.com>
2026-01-08 08:46:33 +0100

    tolto una regola piccolina di stile per admin

--- index.php
+++ index.php
@@ -27,6 +27,7 @@ $routes = [
     // ],
     // 'animali/cani' => [
     //     'file' => __DIR__ . '/src/php/animali.php',
+    //     'params' => [
     // ],
     // 'animali/gatti' => [
     //     'file' => __DIR__ . '/src/php/animali.php',
