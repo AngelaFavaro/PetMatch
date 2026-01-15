dcbb1f1f23c981d961be3f7309a710b94cdc8673
Linor <lindorlinor@gmail.com>
2026-01-15 22:51:52 +0100

    id "description-menu" reso classe perchè era duplicato ops

--- src/utils.php
+++ src/utils.php
@@ -124,10 +124,10 @@ function buildAdminNav(array $menuGroups, string $currentHref): string {
     foreach ($menuGroups as $key => $items) {
 
         if ($key === 'animali') {
-            $html .= '<span id="description-menu" aria-hidden="true">Animali</span>';
+            $html .= '<span class="description-menu" aria-hidden="true">Animali</span>';
             $html .= '<ul aria-label="Menù gestione animali">';
         }else{
-            $html .= '<span id="description-menu" aria-hidden="true">Principale</span>';
+            $html .= '<span class="description-menu" aria-hidden="true">Principale</span>';
             $html .= '<ul aria-label="Menù principale">';
         }
 
