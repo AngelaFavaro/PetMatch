4a2901f55e2623ca08d6556f137156f28ac6fabb
Kibbino <dabik.cure@gmail.com>
2026-01-20 17:42:46 +0100

    sistemato problema hover in preferiti sul cuore

--- src/php/animali.php
+++ src/php/animali.php
@@ -207,13 +207,8 @@ function buildAnimalCards(array $animali, ?string $email): string {
             }
 
             // se adottato → niente interazione
-            if ($adottato) {
-                $classePreferito = 'not-favorite disabled';
-                $statusPreferiti = 'Animale adottato';
-            } else {
-                $classePreferito = $inPreferiti ? 'is-favorite' : 'not-favorite';
-                $statusPreferiti = $inPreferiti ? 'Rimuovi dai preferiti' : 'Aggiungi ai preferiti';
-            }
+            $classePreferito = $inPreferiti ? 'is-favorite' : 'not-favorite';
+            $statusPreferiti = $inPreferiti ? 'Rimuovi dai preferiti' : 'Aggiungi ai preferiti';
             $heartNormal = $inPreferiti ? 'active-like.svg' : 'inactive-like.svg';
             $heartHover  = $inPreferiti ? 'inactive-like.svg' : 'active-like.svg';
 
@@ -247,12 +242,8 @@ function buildAnimalCards(array $animali, ?string $email): string {
                             <button type='submit'
                                     class='$classePreferito'
                                     aria-label='$statusPreferiti'>
-                                <img class='heart-normal' src='./assets/icons/$heartNormal' alt=''>";
-            if(!$inPreferiti) {
-                $html.="
-                                <img class='heart-hover' src='./assets/icons/$heartHover' alt=''>";
-            }
-            $html.="
+                                <img class='heart-normal' src='./assets/icons/$heartNormal' alt=''>
+                                <img class='heart-hover' src='./assets/icons/$heartHover' alt=''>
                             </button>
                         </form>
                     </div>
