07200d33dc122678d73ce1ea0a04174f31566cda
Linor <lindorlinor@gmail.com>
2026-01-25 13:17:19 +0100

    aggiunte ultime modifiche print area personale

--- src/utils.php
+++ src/utils.php
@@ -190,10 +190,10 @@ function buildAdminNav(array $menuGroups, string $currentHref): string {
     
     <nav id="menu-admin" aria-label="Menù">
         <a class="navigationHelp" href="#content"> Salta il menù di navigazione</a>
-        <a href="./home">
-            <img src="./assets/icons/light-mode-logo.svg" id="logo" alt="Home" lang="en">
+        <a href="./home" id="logo-link">
+            <span id="logo" aria-label="Home"></span>
         </a>';
-        $html .= $currentHref==='./nuovo-animale' ? '<p class="orange-button" id="currentLink" href="./nuovo-animale">+ Aggiungi animale</p>' : '<a class="orange-button" href="./nuovo-animale">+ Aggiungi animale</a>';
+        $html .= $currentHref==='./nuovo-animale' ? '<p class="orange-button" id="currentLink" href="./nuovo-animale">+ Aggiungi animale</p>' : '<button class="orange-button" href="./nuovo-animale">+ Aggiungi animale</button>';
 
     foreach ($menuGroups as $key => $items) {
 
