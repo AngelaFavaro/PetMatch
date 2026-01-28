1fa40ef280d1eb739b44bab9a229f621f3b84da2
AngelaFavaro <angela.favaro@studenti.unipd.it>
2026-01-28 23:46:16 +0100

    okay angela accessibilità on point

--- src/php/admin/dettagli-animale.php
+++ src/php/admin/dettagli-animale.php
@@ -199,5 +199,11 @@ $main = str_replace([
 $main = str_replace('[elencoRichieste]', $listRequestHTML, $main);
 
 $paginaHTML = str_replace('[main]', $main, $paginaHTML);
+
+$showModal = $_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['show-dialog']);
+$closeModal = $_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['close-dialog']);
+$paginaHTML = str_replace('[openDialog]', $showModal?'open':'', $paginaHTML);
+$paginaHTML = str_replace('[openDialog]', $closeModal?'':'', $paginaHTML);
+
 echo $paginaHTML;
 ?>
