ddfe65bb56599430672d834d32b0b705cf6435c2
angela <angela.canazza@studenti.unipd.it>
2026-01-28 22:13:21 +0100

    modificato css di visualizza evento

--- src/php/visualizzazione-evento.php
+++ src/php/visualizzazione-evento.php
@@ -64,17 +64,15 @@ function buildMainevent(array $dettagliEvento): string {
 
         // 3. Creazione del blocco HTML (con le variabili ora piene!)
         $html = "<div id='mainEvent'>
-        <h1>$titolo</h1>
         <div id='evento'>
-            <div class='immagine'>
-                <img id='foto-animale' src='$img' alt='Immagine evento $titolo'>
-            </div>
-                <dl>
-                    <dt>Luogo</dt> <dd> $luogo </dd>
-                    <dt>Data</dt> <dd> $dataFormattata </dd>
-                    <dt>Descrizione</dt> <dd>$descrizione</dd>
-                </dl>
-            </div>
+        <img class='square-foto' id='foto-animale' src='$img' alt='Immagine evento $titolo'>
+            <h1>$titolo</h1>
+            <dl>
+                <dt>Luogo</dt> <dd> $luogo </dd>
+                <dt>Data</dt> <dd> $dataFormattata </dd>
+                <dt id='descrizione-evento'>Descrizione</dt> <dd>$descrizione</dd>
+            </dl>
+        </div>
         </div>";
     } else {
         $html = "<p class='errore'>No info</p>";
