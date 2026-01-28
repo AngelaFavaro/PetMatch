547033d237dd368317414fdf73d9df3ca5342f68
angela <angela.canazza@studenti.unipd.it>
2026-01-28 22:26:23 +0100

    modificato per l'accessibilità, tolto l'aria describedby

--- src/php/visualizzazione-evento.php
+++ src/php/visualizzazione-evento.php
@@ -24,7 +24,7 @@ function buildEventsCards($events): string {
         $data=formattaDataItaliana($e['data_evento']);
         $titolo=$e['titolo'];
         $html .= "<li>
-                    <article class='evento' aria-labelledby='evento-titolo'>
+                    <article class='evento'>
                         <!-- Immagine dell'evento -->
                         <img class='immagine-evento' src=$img alt=''>
                         <h2 id='evento-titolo-$titolo'>$titolo</h2>
@@ -65,7 +65,7 @@ function buildMainevent(array $dettagliEvento): string {
         // 3. Creazione del blocco HTML (con le variabili ora piene!)
         $html = "<div id='mainEvent'>
         <div id='evento'>
-        <img class='square-foto' id='foto-animale' src='$img' alt='Immagine evento $titolo'>
+        <img class='square-foto' id='foto-animale' src='$img' alt='foto del luogo per evento  $titolo'>
             <h1>$titolo</h1>
             <dl>
                 <dt>Luogo</dt> <dd> $luogo </dd>
