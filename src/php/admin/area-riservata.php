51c85167de4201b20a033c5b75fb26ea4fc2bf66
AngelaFavaro <angela.favaro@studenti.unipd.it>
2026-01-31 18:21:56 +0100

    Dettaglio-animale done

--- src/php/admin/area-riservata.php
+++ src/php/admin/area-riservata.php
@@ -299,7 +299,7 @@ if (empty($adminInfo['ImgPath']) || !file_exists($adminInfo['ImgPath'])) {
 $paginaHTML = loadTemplate('./src/template/layout-admin.html', '<p>Errore: <span lang="en">template layout</span>.html non trovato o non leggibile.</p>');
 
 $title = '<title>Area riservata <span lang="en">admin</span> - <span lang="en">PetMatch</span></title>';
-$description = '<meta name="description" content="Area riservata per gli amministratori di <span lang="en">PetMatch</span>">';
+$description = '<meta name="description" content="Area riservata per gli amministratori di PetMatch">';
 $keywords = "<meta name='keywords' content='amministratore, area riservata, PetMatch'>";
 
 
