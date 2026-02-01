2171d8988f2d2dcab456480bf337b6e0a69e8a04
Kibbino <dabik.cure@gmail.com>
2026-02-01 03:01:26 +0100

    sistemato cookie preferiti, ora solo se non sei admin si aggiornano

--- src/php/accedi.php
+++ src/php/accedi.php
@@ -55,36 +55,39 @@ function checkCredential(DBAccess $conn, &$email) {
         if (empty($errors)) {
             $_SESSION['email'] = $email;
             // === MIGRAZIONE PREFERITI DA COOKIE A DB ===
-            if (isset($_COOKIE['preferiti_guest'])) {
-
-                $preferiti = json_decode($_COOKIE['preferiti_guest'], true);
-
-                if (is_array($preferiti) && !empty($preferiti)) {
-
-                    foreach ($preferiti as $idAnimale) {
-                        $idAnimale = (int)$idAnimale;
-
-                        // evita duplicati
-                        if (!$conn->isAnimalInFavorites($email, $idAnimale)) {
-                            $conn->addToFavorites($email, $idAnimale);
-                        }
-                    }
-                }
-
-                // cancella cookie dopo migrazione
-                setcookie('preferiti_guest', '', time() - 3600, '/');
-            }
-
-
+            
             $role = $conn->getRole($email);
 
             if ($role === 'Admin') {
                 $_SESSION['admin'] = true;
                 $_SESSION['ruolo'] = 'Admin';
+                if (isset($_COOKIE['preferiti_guest'])) { //cancello cookies
+                    setcookie('preferiti_guest', '', time() - 3600, '/');
+                }
                 header("Location: ./area-riservata");
             } else if($role === 'User'){
                 $_SESSION['admin'] = false;
                 $_SESSION['ruolo'] = 'User';
+                if (isset($_COOKIE['preferiti_guest'])) {
+
+                    $preferiti = json_decode($_COOKIE['preferiti_guest'], true);
+
+                    if (is_array($preferiti) && !empty($preferiti)) {
+
+                        foreach ($preferiti as $idAnimale) {
+                            $idAnimale = (int)$idAnimale;
+
+                            // evita duplicati
+                            if (!$conn->isAnimalInFavorites($email, $idAnimale)) {
+                                $conn->addToFavorites($email, $idAnimale);
+                            }
+                        }
+                    }                
+                }
+            // cancella cookie dopo migrazione
+            if (isset($_COOKIE['preferiti_guest'])) {
+                setcookie('preferiti_guest', '', time() - 3600, '/');
+            }
                 header("Location: ./profilo-utente"); 
             }
             exit;
