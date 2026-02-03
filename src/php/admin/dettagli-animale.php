99b0f73a80585591d1f3325664d40a77f5c153ae
AngelaFavaro <angela.favaro@studenti.unipd.it>
2026-02-03 09:56:49 +0100

    migliorato codice visualizzazione animale admin

--- src/php/admin/dettagli-animale.php
+++ src/php/admin/dettagli-animale.php
@@ -178,7 +178,7 @@ $paginaHTML = str_replace(['[breadcrumb]', '[title]', '[nav]', '[description]', 
 $main = str_replace('[idAnimale]', e($idAnimale), $main);
 $main = str_replace('[nomeAnimale]', e($richiesta['Nome'] ?? 'N/D'), $main);
 $imgPath = $richiesta['ImgPath'] ?? '';
-if (!$imgPath || !file_exists($imgPath)) {
+if (empty($imgPath) || !file_exists($imgPath)) {
     $imgPath = (isset($richiesta['Tipo']) && $richiesta['Tipo'] === 'Gatto') 
                ? './assets/images/animals/defaultGatto.jpg' 
                : './assets/images/animals/defaultCane.jpg';
@@ -199,18 +199,10 @@ $main = str_replace('[trasportoAnimale]', siNo($richiesta['Trasporto'] ?? 0), $m
 $main = str_replace('[famigliaIdeale]', e($richiesta['DescrFamiglia'] ?? 'N/D'), $main);
 $main = str_replace('[descrizioneCaratteriale]', e($richiesta['DescrComportamentale'] ?? 'N/D'), $main);
 
-$condizioni = ($richiesta['CondizioniMediche'] === null || $richiesta['CondizioniMediche'] === '0' || empty(trim($richiesta['CondizioniMediche']))) 
+$condizioni = (empty(trim($richiesta['CondizioniMediche'] ?? '')) || $richiesta['CondizioniMediche'] === '0') 
     ? '<em class="no-data">Nessuna</em>' 
     : e($richiesta['CondizioniMediche']);
 $main = str_replace('[condizioniMediche]', $condizioni, $main);
-
-$urlModifica = $pagine['modifica-animale']['url'] . "?id-animale=" . urlencode($idAnimale);
-if ($from) {
-    $urlModifica .= "&from=" . urlencode($from);
-}
-if (isset($_GET['from_email'])) {
-    $urlModifica .= "&from_email=" . urlencode($_GET['from_email']);
-}
 
 $main = str_replace('[pulsanti-modifica-animale]', $btnModifica, $main);
 $main = str_replace('[pulsante-elimina-animale]', $btnEliminaHTML, $main); 
