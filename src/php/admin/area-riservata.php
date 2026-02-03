dad6281d487776a970e86082e18e8cedc03fb490
AngelaFavaro <angela.favaro@studenti.unipd.it>
2026-02-03 09:51:58 +0100

    tolte session_start inutili

--- src/php/admin/area-riservata.php
+++ src/php/admin/area-riservata.php
@@ -2,7 +2,6 @@
 include './src/utils.php';
 include './src/DBconnection.php';
 use DB\DBAccess;
-session_start();
 
 //il primo controlla se esiste la variabile admin in session, la seconda controlla che sia affettivamente admin
 if (!isset($_SESSION['admin']) || $_SESSION['admin'] !== true) { 
